5b45794ebc23d1f1402a03c5c5e8de1fe315f4f1
anuveshkumar <anuvesh.kumar@gmail.com>
2019-05-26 20:57:27 +0530

    Update DiscriminativeModel.py

--- DiscriminativeModel.py
+++ DiscriminativeModel.py
@@ -62,44 +62,44 @@ while no_games < 100:
     Y_padded.append(y[this_game])
     no_games += 1
 
-#
-# from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(np.array(X_padded), np.array(Y_padded), test_size = 0.25, random_state = 1)
-#
-#
-# batch_size = 64
-#
-# # Generating the Model
-# from keras.models import Sequential
-# from keras.layers import Embedding, LSTM, Dense, CuDNNLSTM, CuDNNGRU
-# vocabulary_size = len(tk.word_counts.keys())+1
-# max_words = maxlen
-# embedding_size = 64
-# model = Sequential()
-# model.add(Embedding(vocabulary_size, embedding_size, input_length=max_words))
-# model.add(CuDNNLSTM(64, return_sequences=True))
-# model.add(CuDNNLSTM(64, return_sequences=True))
-# model.add(CuDNNLSTM(64, return_sequences=True))
-# model.add(CuDNNLSTM(64, return_sequences=True))
-# model.add(CuDNNLSTM(64, return_sequences=True))
-# model.add(CuDNNLSTM(64, return_sequences=False))
-# # model.add(CuDNNLSTM(128, return_sequences=True))
-# # model.add(CuDNNLSTM(128, return_sequences=False))
-# model.add(Dense(1, activation='sigmoid'))
-# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# model.summary()
-#
-# # Validation & Early Stopping
-# from keras.callbacks import ModelCheckpoint, EarlyStopping
-#
-# filepath = 'discriminate_weights_good1mil50_64_64_6x64.hdf5'
-# early_stop_checkpoint = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=2, min_delta=0)
-# save_checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='min', period=1)
-# callbacks_list = [early_stop_checkpoint, save_checkpoint]
-#
-# # Training the Model
-# foo = model.fit(X_train, y_train, validation_split=0.05, batch_size=batch_size, epochs=1, callbacks=callbacks_list)
-# print(foo)
-# # Testing the Model
-# scores = model.evaluate(X_test, y_test, batch_size=128, verbose=0)
-# print("Test accuracy", scores[1])
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(np.array(X_padded), np.array(Y_padded), test_size = 0.25, random_state = 1)
+
+
+batch_size = 64
+
+# Generating the Model
+from keras.models import Sequential
+from keras.layers import Embedding, LSTM, Dense, CuDNNLSTM, CuDNNGRU
+vocabulary_size = len(tk.word_counts.keys())+1
+max_words = maxlen
+embedding_size = 64
+model = Sequential()
+model.add(Embedding(vocabulary_size, embedding_size, input_length=max_words))
+model.add(CuDNNLSTM(64, return_sequences=True))
+model.add(CuDNNLSTM(64, return_sequences=True))
+model.add(CuDNNLSTM(64, return_sequences=True))
+model.add(CuDNNLSTM(64, return_sequences=True))
+model.add(CuDNNLSTM(64, return_sequences=True))
+model.add(CuDNNLSTM(64, return_sequences=False))
+# model.add(CuDNNLSTM(128, return_sequences=True))
+# model.add(CuDNNLSTM(128, return_sequences=False))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+# Validation & Early Stopping
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+filepath = 'discriminate_weights_good1mil50_64_64_6x64.hdf5'
+early_stop_checkpoint = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=2, min_delta=0)
+save_checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False, mode='min', period=1)
+callbacks_list = [early_stop_checkpoint, save_checkpoint]
+
+# Training the Model
+foo = model.fit(X_train, y_train, validation_split=0.05, batch_size=batch_size, epochs=1, callbacks=callbacks_list)
+print(foo)
+# Testing the Model
+scores = model.evaluate(X_test, y_test, batch_size=128, verbose=0)
+print("Test accuracy", scores[1])
